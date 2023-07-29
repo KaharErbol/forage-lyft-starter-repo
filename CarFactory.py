d737071650f2fc19db70925588a93c0a3e3254bb
KaharErbol <kaharerbol@gmail.com>
2023-07-29 15:27:26 -0700

    Added Tire Criteria and test cases for all car model

--- CarFactory.py
+++ CarFactory.py
@@ -2,6 +2,7 @@ from Car import Car
 from datetime import datetime
 from Engine import CapuletEngine, WilloughbyEngine, SternmanEngine
 from Battery import SpindlerBattery, NubbinBattery
+from Tire import Carrigan, Octoprime
 
 class CarFactory:
     def date_str_convert(self, date):
@@ -9,57 +10,91 @@ class CarFactory:
         return converted_date
 
 
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(self, 
+                        current_date, 
+                        last_service_date, 
+                        current_mileage, 
+                        last_service_mileage, 
+                        sensor_data
+                        ):
         curr_date = self.date_str_convert(current_date)
         last_date = self.date_str_convert(last_service_date)
 
         calliope_engine = CapuletEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
         calliope_battery = SpindlerBattery(last_service_date=last_date,current_date=curr_date)
+        calliope_tire = Carrigan(sensor_data)
 
-        calliope = Car(engine=calliope_engine, battery=calliope_battery)
+        calliope = Car(engine=calliope_engine, battery=calliope_battery, tire=calliope_tire)
 
         return calliope
     
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(self, 
+                        current_date, 
+                        last_service_date, 
+                        current_mileage, 
+                        last_service_mileage, 
+                        sensor_data
+                        ):
         curr_date = self.date_str_convert(current_date)
         last_date = self.date_str_convert(last_service_date)
 
         glissade_engine = WilloughbyEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
         glissade_battery = SpindlerBattery(last_service_date=last_date,current_date=curr_date)
+        glissade_tire = Carrigan(sensor_data)
 
-        glissade = Car(engine=glissade_engine, battery=glissade_battery)
+        glissade = Car(engine=glissade_engine, battery=glissade_battery, tire=glissade_tire)
 
         return glissade
     
-    def create_plaindrome(self, current_date, last_service_date, warning_light_on):
+    def create_plaindrome(self, 
+                          current_date, 
+                          last_service_date, 
+                          warning_light_on, 
+                          sensor_data
+                          ):
         curr_date = self.date_str_convert(current_date)
         last_date = self.date_str_convert(last_service_date)
 
         plaindrome_engine = SternmanEngine(warning_light_on=warning_light_on)
         plaindrome_battery = SpindlerBattery(last_service_date=last_date,current_date=curr_date)
+        plaindrome_tire = Octoprime(sensor_data)
 
-        plaindrome = Car(engine=plaindrome_engine, battery=plaindrome_battery)
+        plaindrome = Car(engine=plaindrome_engine, battery=plaindrome_battery, tire=plaindrome_tire)
 
         return plaindrome
     
-    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_rorschach(self, 
+                         current_date, 
+                         last_service_date, 
+                         current_mileage, 
+                         last_service_mileage, 
+                         sensor_data
+                         ):
         curr_date = self.date_str_convert(current_date)
         last_date = self.date_str_convert(last_service_date)
 
         rorschach_engine = CapuletEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
         rorschach_battery = NubbinBattery(last_service_date=last_date, current_date=curr_date)
+        rorschach_tire = Octoprime(sensor_data)
 
-        rorschach = Car(engine=rorschach_engine, battery=rorschach_battery)
+        rorschach = Car(engine=rorschach_engine, battery=rorschach_battery, tire=rorschach_tire)
 
         return rorschach
     
-    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_thovex(self, 
+                      current_date, 
+                      last_service_date, 
+                      current_mileage, 
+                      last_service_mileage, 
+                      sensor_data
+                      ):
         curr_date = self.date_str_convert(current_date)
         last_date = self.date_str_convert(last_service_date)
 
         thovex_engine = WilloughbyEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
         thovex_battery = NubbinBattery(last_service_date=last_date, current_date=curr_date)
+        thovex_tire = Octoprime(sensor_data)
 
-        thovex = Car(engine=thovex_engine, battery=thovex_battery)
+        thovex = Car(engine=thovex_engine, battery=thovex_battery, tire=thovex_tire)
 
         return thovex
--- test_car.py
+++ test_car.py
@@ -2,12 +2,6 @@ from unittest import TestCase
 from CarFactory import CarFactory
 
 
-"""
-Car Model       Engine	       Battery
-Calliope	Capulet Engine	Spindler Battery
-
-"""
-
 
 class TestCar(TestCase):
     def test_calliope(self):
@@ -18,12 +12,14 @@ class TestCar(TestCase):
         last_service = '2020-01-01'
         curr_mileage = 50000
         last_mileage = 20000
+        data = [1,1,1,1]
 
         calliope = create_car.create_calliope(
             current_date=current_date,
             last_service_date=last_service,
             current_mileage=curr_mileage,
-            last_service_mileage=last_mileage
+            last_service_mileage=last_mileage,
+            sensor_data=data
             )
 
         # Act
@@ -31,3 +27,98 @@ class TestCar(TestCase):
 
         # Assert
         self.assertEqual(result, True)
+
+    def test_glissade(self):
+        # Arrange
+        create_car = CarFactory()
+        
+        current_date = '2023-7-29'
+        last_service = '2020-01-01'
+        curr_mileage = 50000
+        last_mileage = 20000
+        data = [1,1,1,1]
+
+        glissade = create_car.create_glissade(
+            current_date=current_date,
+            last_service_date=last_service,
+            current_mileage=curr_mileage,
+            last_service_mileage=last_mileage,
+            sensor_data=data
+        )
+
+        # Act
+        result = glissade.needs_service()
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_plaindrome(self):
+        # Arrange
+        create_car = CarFactory()
+        
+        current_date = '2023-7-29'
+        last_service = '2020-01-01'
+        warning = True
+        data = [1,1,1,1]
+
+        plaindrome = create_car.create_plaindrome(
+            current_date=current_date,
+            last_service_date=last_service,
+            warning_light_on=warning,
+            sensor_data=data
+        )
+
+        # Action
+        result = plaindrome.needs_service()
+
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_rorschach(self):
+        # Arrange 
+        create_car = CarFactory()
+        
+        current_date = '2023-7-29'
+        last_service = '2020-01-01'
+        curr_mileage = 50000
+        last_mileage = 20000
+        data = [1,1,1,1]
+
+        rorschach = create_car.create_rorschach(
+            current_date=current_date,
+            last_service_date=last_service,
+            current_mileage=curr_mileage,
+            last_service_mileage=last_mileage,
+            sensor_data=data
+        )
+
+        # Act
+        result = rorschach.needs_service()
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_thoves(self):
+        # Arrange
+        create_car = CarFactory()
+        
+        current_date = '2023-7-29'
+        last_service = '2020-01-01'
+        curr_mileage = 50000
+        last_mileage = 20000
+        data = [1,1,1,1]
+
+        thovex = create_car.create_thovex(
+            current_date=current_date,
+            last_service_date=last_service,
+            current_mileage=curr_mileage,
+            last_service_mileage=last_mileage,
+            sensor_data=data
+        )
+
+        # Act
+        result = thovex.needs_service()
+
+        # Assert
+        self.assertTrue(result)
